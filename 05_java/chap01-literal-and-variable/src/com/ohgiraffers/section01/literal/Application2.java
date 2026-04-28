c4b8ef9d4c28420ed462574d8e4d488b97ec22cf
nmhyrhn <gbs06193@gmail.com>
2026-04-28 17:05:16 +0900

    260428 TIL

--- 05_java/chap01-literal-and-variable/src/com/ohgiraffers/section01/literal/Application2.java
+++ 05_java/chap01-literal-and-variable/src/com/ohgiraffers/section01/literal/Application2.java
@@ -21,6 +21,20 @@ public class Application2 {
         System.out.println('a' + 'b'); // 내부적으로 숫자 취급을 한다(아스키코드와 유니코드)
 
         /* 문자열의 연산 */
-        
+        // 문자열과 문자열의 '+' 연산 결과는 문자열 합치기가 된다.
+        System.out.println("hello" + "world");
+//        System.out.println("hello" - "world"); // + 연산 외에 다른 연산은 사용하지 못함
+
+        System.out.println("hello" + 123); // 다른 형태의 값과 + 연산도 문자열 합치기 결과가 나온다
+        System.out.println("hello" + 'a');
+
+        /* 논리값의 연산 */
+        // 모든 연산자 사용 불가
+//        System.out.println(true + false);
+//        System.out.println(true + 1);
+//        System.out.println(true + 1.0);
+//        System.out.println(true + 'a');
+
+        System.out.println(true + "a"); // 유일하게 문자열은 합치기가 가능하다
     }
 }
