513b3bf880d5453c9765e7d3b980c06fe6698e6d
nmhyrhn <gbs06193@gmail.com>
2026-06-08 13:47:34 +0900

    260608

--- 10_mybatis/chap03_crud/src/main/java/com/ohgiraffers/section03/remix/MenuMapper.java
+++ 10_mybatis/chap03_crud/src/main/java/com/ohgiraffers/section03/remix/MenuMapper.java
@@ -4,6 +4,15 @@ import java.util.List;
 
 public interface MenuMapper {
 
+    /*
+    * MenuMapper.java -> MenuMapper.xml 연결 규칙
+    * - XNL이 인터페이스와 동일한 패키지 경로에 위치해야 한다.
+    * - XML의 namespace = 인터페이스의 풀 클래스명
+    * - XML의 파일명 = 인터페이스 파일명과 일치
+    * - XML의 id = 인터페이스의 메서드명과 일치
+    * */
+
+
     List<MenuDTO> selectAllMenu();
 
     MenuDTO selectMenuByCode(int code);
